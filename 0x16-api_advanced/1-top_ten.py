5cca064f8def0767ebde5a9ab78cb4f0d535f14a
abel mesfin <abelmesfin555@gmail.com>
2023-01-31 15:15:10 +0300

    requirement error fix

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,10 +6,14 @@ import requests
 def top_ten(subreddit):
     """..."""
     headers = {"User-Agent": "MyAPI/0.0.2"}
+    params = {
+            "limit": 10
+            }
     res = requests.get('https://www.reddit.com/r/' + subreddit +
-                       '/hot.json?limit=10', headers=headers, allow_redirects=False)
+                       '/hot.json?limit=10', params=params, headers=headers,
+                       allow_redirects=False)
 
     if res.status_code != 200:
         return ("None")
-    for i in range(10):
-        print(res.json()['data']['children'][i]['data']['title'])
+    for i in res.json()['data']['children']:
+        print(i['data']['title'])
