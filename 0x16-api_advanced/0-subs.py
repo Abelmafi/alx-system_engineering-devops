15e4adb04c08f9970cb6516da1ac4ec48256e281
abel mesfin <abelmesfin555@gmail.com>
2023-01-31 13:16:21 +0300

    inital commit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Api request to reddit about sub reddit subscribe count"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """..."""
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+    headers = {"User-Agent": "MyAPI/0.01"}
+    res = requests.get('https://www.reddit.com/r/' + subreddit +
+                       '/about.json', headers=headers)
+
+    return res.json()['data']['subscribers']
