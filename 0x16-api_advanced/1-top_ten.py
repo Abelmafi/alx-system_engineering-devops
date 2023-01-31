eb55678beb48220887d685d931781b0cafb98f97
abel mesfin <abelmesfin555@gmail.com>
2023-01-31 13:34:29 +0300

    inital commit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Api request to reddit about sub reddit subscribe count"""
+import requests
+
+
+def top_ten(subreddit):
+    """..."""
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+    headers = {"User-Agent": "MyAPI/0.0.2"}
+    res = requests.get('https://www.reddit.com/r/' + subreddit +
+                       '/hot.json', headers=headers)
+
+    for i in range(10):
+        print(res.json()['data']['children'][i]['data']['title'])
